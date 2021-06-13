be5942ecec5206af2097adbe4e419e407f71be82
sandeep <sandeepchoudhary0901@gmail.com>
2021-06-14 00:57:49 +0530

    final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
 import {Container,Row,Col} from "react-bootstrap"
 import './Footer.css'
 import white from '../assets/img/white.png'
+import insta from '../assets/img/insta.png'
+import linkedln from '../assets/img/linkedln.png'
+import fb from '../assets/img/fb.png'
+
+
 function Footer() {
     return (
         <div className="footer">
@@ -9,6 +14,7 @@ function Footer() {
                     <Col className="footerLogo footerCol">
                         <img className="white" alt="white" src={white}/>
                         <p className="text">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.</p>
+                        <p className="Copyright">Copyright 2020 Nature</p>
                     </Col>
                     <Col className="info footerCol">
                         <p className="infoHeading">Information</p>
@@ -30,7 +36,12 @@ function Footer() {
                         </Row>
                     </Col> 
                     <Col className="footerCol">
-                        <p>Follow Us</p>  
+                        
+                    <p Follow className="follow">Follow Us</p>  
+
+                        <img className="insta" alt="insta" src={insta}/>
+                        <img className="linkedln" alt="linkedln" src={linkedln}/>
+                        <img className="fb" alt="fb" src={fb}/>
                     </Col>
                 </Row>
             </Container>
